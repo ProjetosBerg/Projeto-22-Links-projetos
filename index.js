5385f1d50a39b1f8a49280f2e9754c9e7bd91c9b
Bergkley <bergkley@gmail.com>
2024-09-25 13:10:48 -0300

    feat: Links para projetos backend

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(cors());
+app.use(cors({ origin: 'https://projeto-22-links-projetos-backend-8c10jgf92.vercel.app/' }));
+
 app.use(express.json());
-app.use(express.static("public"));
+
 
 require("./db/conn");
 
